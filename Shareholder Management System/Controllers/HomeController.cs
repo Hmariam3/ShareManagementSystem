e093837e3ed7495c356a9176c0216dd87a46d660
dannishuuu <danielgelandu1@gmail.com>
2024-09-24 16:03:59 +0300

    Home Controller

--- Shareholder Management System/Controllers/HomeController.cs
+++ Shareholder Management System/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace Shareholder_Management_System.Controllers
 
         public ActionResult Index()
         {
+            //User table
             ViewBag.countUser = db.Users.Count();
             ViewBag.countActive = db.Users.Where(c => c.Status == true).Count();
             ViewBag.countInactive = db.Users.Where(c => c.Status == false).Count();
@@ -20,6 +21,69 @@ namespace Shareholder_Management_System.Controllers
             ViewBag.countAdmin = db.Users.Where(c => c.Role == "Administrator").Count();
             ViewBag.countIntiator = db.Users.Where(c => c.Role == "Initiator").Count();
             ViewBag.countAuthorizer = db.Users.Where(c => c.Role == "Authorizer").Count();
+
+            //Share Holders Table
+            ViewBag.countShare = db.Shareholders.Count();
+
+            var totalShareholder = ViewBag.countShare;
+
+            // Get the current year
+            var currentYear = DateTime.Now.Year;
+
+            // Get the count of shareholders registered in the current year
+            var currentYearShareholders = db.Shareholders
+                .Where(st => st.CreatedDate.Value.Year == currentYear)
+                .Count();
+
+            double percentageCurrentYearShareholders = (double)currentYearShareholders / totalShareholder * 100;
+
+            ViewBag.PercentageCurrentYearShareholders = percentageCurrentYearShareholders;
+
+            //Subscribtions Table
+            ViewBag.countSub = db.Subscribtions.Count();
+
+            var totalSub = ViewBag.countSub;
+
+            var currentYearSub = DateTime.Now.Year;
+
+            var currentYearSubs = db.Subscribtions
+                .Where(su => su.SubDate.Value.Year == currentYearSub)
+                .Count();
+
+            double percentageCurrentYearSubs = (double)currentYearSubs / totalSub * 100;
+
+            ViewBag.PercentageSubs = percentageCurrentYearSubs;
+
+            //Payment Table
+            ViewBag.countPay = db.Payments.Count();
+
+            var totalPay = ViewBag.countPay;
+
+            var currentYearPay = DateTime.Now.Year;
+
+            var currentYearPays = db.Payments
+                .Where(pa => pa.PaymentDate.Value.Year == currentYearPay)
+                .Count();
+
+            double percentageCurrentYearPay = (double)currentYearPays / totalPay * 100;
+
+            ViewBag.PercentagePay = percentageCurrentYearPay;
+
+            //Transfer Table
+            ViewBag.countTran = db.ShareTransfers.Count();
+
+            var totalTranfer = ViewBag.CountTran;
+
+            var currentYearTran = DateTime.Now.Year;
+
+            var currentYearTrans = db.ShareTransfers
+                .Where(tr => tr.CreationDate.Value.Year == currentYearTran)
+                .Count();
+
+            double percentageCurrentYearTran = (double)currentYearTrans / totalTranfer * 100;
+
+            ViewBag.percentageTrans = percentageCurrentYearTran;
+
             return View();
         }
 
