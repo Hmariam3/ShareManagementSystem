5f1ec2775dd4adcb52dd3bf2817b3820a8ec1f2b
Thomasgme <tomgkme@gmail.com>
2024-10-08 13:55:57 +0300

    tt

--- Shareholder Management System/Controllers/PaymentsController.cs
+++ Shareholder Management System/Controllers/PaymentsController.cs
@@ -91,6 +91,7 @@ namespace Shareholder_Management_System.Controllers
         }
 
 
+
         // POST: Payments/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
