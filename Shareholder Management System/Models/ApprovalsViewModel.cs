c42735e876434ebc337b351487efc25dbd18f944
Abel Birhane <38845819+avela1@users.noreply.github.com>
2024-12-02 10:10:50 +0300

    Create ApprovalsViewModel.cs
    creating approval's view model

---
+++ Shareholder Management System/Models/ApprovalsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shareholder_Management_System.Models
+{
+    public class ApprovalsViewModel
+    {
+        public IEnumerable<Shareholder> Shareholders { get; set; }
+        public IEnumerable<Proxy> Proxies { get; set; }
+        public IEnumerable<Subscribtion> Subscribtions { get; set; }
+        public IEnumerable<Payment> Payments { get; set; }
+        public IEnumerable<ShareTransfer> ShareTransfers { get; set; }
+        public IEnumerable<Blocked> Blockeds { get; set; }
+        public IEnumerable<Document> Documents { get; set; }
+
+    }
+}
