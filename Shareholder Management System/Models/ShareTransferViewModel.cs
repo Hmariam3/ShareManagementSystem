6f91e334a59052dd2302463887f747228030199d
hailemariamk <hmariamkebede3@gmailcom>
2026-04-06 05:08:29 -0700

    Push changes from jump server

---
+++ Shareholder Management System/Models/ShareTransferViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shareholder_Management_System.Models
+{
+    public class ShareTransferViewModel
+    {
+        public IQueryable<ShareTransfer> ShareTransfers { get; set; }
+        public SelectList ShareholderList { get; set; }
+        public string SelectedShareholder { get; set; }
+    }
+}
+
+
---
+++ Shareholder Management System/Models/SubscriptionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shareholder_Management_System.Models
+{
+    public class SubscriptionViewModel
+    {
+        public IQueryable<Subscribtion> Subscriptions { get; set; }
+        public SelectList ShareholderList { get; set; }
+        public string SelectedShareholder { get; set; }
+    }
+}
