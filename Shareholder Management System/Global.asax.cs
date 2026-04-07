b2cd02ed9b8e796c74d5d216cfd363decdd71f3c
hailemariamk <hmariamkebede3@gmailcom>
2026-04-07 04:45:59 -0700

    Metrics Exposed From Jump Server

--- Shareholder Management System/Global.asax.cs
+++ Shareholder Management System/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using Shareholder_Management_System.commons;
 namespace Shareholder_Management_System
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -19,9 +19,27 @@ namespace Shareholder_Management_System
         }
         protected void Application_BeginRequest()
         {
+            HttpContext.Current.Items["RequestStartTime"] = DateTime.UtcNow;
+            HttpContext.Current.Response.Headers.Add("Content-Security-Policy",
+             "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';");
             Response.Cache.SetCacheability(HttpCacheability.NoCache);
             Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
             Response.Cache.SetNoStore();
+        }
+
+        protected void Application_EndRequest()
+        {
+            var context = HttpContext.Current;
+
+            var start = (DateTime)context.Items["RequestStartTime"];
+            var duration = DateTime.UtcNow - start;
+
+            var endpoint = context.Request.Url.AbsolutePath;
+            var method = context.Request.HttpMethod;
+            var status = context.Response.StatusCode.ToString();
+
+            AppMetrics.HttpRequests.WithLabels(method, endpoint, status).Inc();
+            AppMetrics.HttpRequestDuration.WithLabels(endpoint).Observe(duration.TotalSeconds);
         }
     }
 }
---
+++ Shareholder Management System/commons/AppMetrics.cs
@@ -0,0 +1,26 @@
+﻿using Prometheus;
+
+public static class AppMetrics
+{
+
+
+    public static readonly Counter HttpRequests =
+        Metrics.CreateCounter(
+            "sms_http_requests_total",
+            "Total HTTP requests",
+            new CounterConfiguration
+            {
+                LabelNames = new[] { "method", "endpoint", "status" }
+            });
+
+    public static readonly Histogram HttpRequestDuration =
+        Metrics.CreateHistogram(
+            "sms_http_request_duration_seconds",
+            "HTTP request duration",
+            new HistogramConfiguration
+            {
+                LabelNames = new[] { "endpoint" }
+            });
+
+
+}
